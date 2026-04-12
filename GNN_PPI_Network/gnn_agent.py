094594e56308ef7406ee541f4bd1f355a8aaa5e3
Zander-Rothering <zander_rothering@berkeley.edu>
2026-04-12 17:31:51 -0500

    Updated to train on torch object

--- GNN_PPI_Network/gnn_agent.py
+++ GNN_PPI_Network/gnn_agent.py
@@ -1,28 +1,43 @@
+import torch
 from torch_geometric.data import Data
 
 from gnn_model import GNNModel
-from node_label import node_labels
+from node_label import node_labeler
 from node_feature_builder import feature_extracter
 from ppi_graph_builder import PPIGraphBuilder
 
-# Get edgeindex of all proteins
-ppi_g_build = PPIGraphBuilder()
-edge_index = ppi_g_build.merge_networks()
+class gnn_agent():
+    def __init__(self):
+        builder = PPIGraphBuilder()
+        stringdb_df = builder.get_stringdb_network()
+        biogrid_df = builder.get_biogrid_network()
+        edges_df = builder.merge_networks(stringdb_df, biogrid_df)
+        proteins, protein_mapping = builder.protein_extraction(edges_df) 
+        edge_index = builder.build_edges(edges_df, protein_mapping)
+        edge_attr = builder.edge_weights(edges_df)
 
-# Extract protein feature data
-fe = feature_extracter()
-x = fe.get_biological_features()
 
-# Get actual labels for proteins
-y = node_labels(x) # Need to drop lables data from features prior to training!
+        extractor = feature_extracter(proteins)
+        protein_features_df = extractor.get_uniprot()
+        x = extractor.node_features_encoder(protein_features_df)
 
-data = Data(x=x, edge_index=edge_index, y=y)
+        
+        labeler = node_labeler(proteins)
+        y = labeler.node_labels()
 
-model = GNNModel(in_channels=data.num_node_features, hidden_channels=128, num_classes=2)
+        data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
+        self.data = data
+        
+        model = GNNModel(in_channels=data.num_node_features, hidden_channels=128, num_classes=2)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-loss_function = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+        loss_function = nn.CrossEntropyLoss()
 
-trainer = GNNtrainer(model, optimizer, loss_function)
-for epoch in range(1000):
-    loss = trainer.train(data)
+        trainer = GNNtrainer(model, optimizer, loss_function)
+        self.trainer = trainer
+
+    def train_model(self, epochs=1000):
+            for epoch in range(epochs):
+                loss = self.trainer.train(self.data)
+
+            return loss
