3d16ae7f8769da87b2c605666f35a3ce97e9af82
Zander-Rothering <zander_rothering@berkeley.edu>
2026-04-12 16:38:19 -0500

    Updated node_label

--- GNN_PPI_Network/node_label.py
+++ GNN_PPI_Network/node_label.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import requests
+import torch
 class node_labeler():
     def __init__(self, protein_gene_file= "proteins.txt"):
         """
@@ -11,43 +11,40 @@ class node_labeler():
         
         self.protein_genes = protein_genes
 
-    def get_disgenet(self, api_key='37587e7d-5f2c-4434-a3c6-54fad187142b', save_path="disgenet_features.csv"):
-            
-            base_url = "https://disgenet.com"
-            headers = {"api_key": api_key, "Accept": "application/json"}
+    def get_disgenet(self, input_path= 'disgenet_features.csv'):
+        
+        disgenet_df = pd.read_csv(input_path)
 
-            disgenet_features = []
+        disgenet_df['score'] = pd.to_numeric(disgenet_df['score'], errors='coerce').fillna(0)
+        
+        return disgenet_df
 
-            for p in self.proteins:
-                disgenet_response = requests.get(f"{base_url}/{p}", headers=headers)
-                    
-                disgenet_data = disgenet_response.json()
-                disgenet_results = disgenet_data.get("results", []) 
-                disgenet_df = pd.json_normalize(disgenet_results)
-                disgenet_features.append(disgenet_df )
+    def clean_disgenet(self):
 
-                disgenet_proteins_df = pd.concat(disgenet_features, ignore_index=True)
-                disgenet_proteins_df.to_csv(save_path, index=False)
-            
-            return disgenet_proteins_df
+        disgenet_df = self.get_disgenet()
 
-    def clean_disgenet():
-        pass
+        disgenet_scores_df = disgenet_df.groupby('geneSymbol', as_index=False)['score'].max()
 
-    def node_labels(protein_features, disease_score_threshold=0.3):
+        return disgenet_scores_df
+
+    def node_labels(self, disease_score_threshold=0.3):
         """
         Creates label for GNN training.
         1 = Strong disease association (High Risk)
         0 = Weak or no association
         """
+        proteins_df = self.clean_disgenet()
 
-        # Fill NaNs with 0 (No associations or no data)
-        protein_features['score'] = pd.to_numeric(protein_features['score'], errors='coerce').fillna(0)
+        # Create label for proteins based on disease score
+        proteins_df['label'] = (proteins_df['score'] >= disease_score_threshold).astype(int)
 
-        # Create labels for proteins based on disease score
-        protein_features['label'] = (protein_features['score'] >= disease_score_threshold).astype(int)
+        # Map gene symbol to label for aligning to protein order
+        gene_to_label = dict(zip(proteins_df['geneSymbol'], proteins_df['label']))
 
-        # Create single label for protein based on max labels score
-        protein_label = protein_features.groupby('accession')['label'].max()
+        # Ensures labels are aligned with protein order
+        labels = [gene_to_label.get(gene, 0) for gene in self.protein_genes]
 
-        return protein_label
+        # Convert labels to torch tensor
+        y_tensor = torch.tensor(labels, dtype=torch.long)
+
+        return y_tensor
