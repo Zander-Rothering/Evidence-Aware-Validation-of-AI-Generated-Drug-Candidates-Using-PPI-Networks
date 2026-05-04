d7ff26c2e9e228fc3eb847e022e071a334787058
Zander-Rothering <zander_rothering@berkeley.edu>
2026-05-04 18:33:48 -0500

    Created initial Evidence_Aggregator files

---
+++ Evidence_Aggregation/risk_weight_config.py
@@ -0,0 +1,9 @@
+@dataclass
+class RiskWeightConfig:
+    """
+
+    """
+    def __init__(self, weight_cme, weight_nlp, weight_gnn):
+        self.weights_cme = weight_cme
+        self.weights_nlp = weight_nlp
+        self.weights_gnn = weight_gnn
