38e85563b0e135a20d6fdb6663446cf82b92ff51
Dongwan Kim <dongwan1229@gmail.com>
2026-05-06 07:30:23 -0700

    Update risk_weight_config.py: update evidence aggregation weight config to 0.45, 0.25, 0.30

--- Evidence_Aggregation/risk_weight_config.py
+++ Evidence_Aggregation/risk_weight_config.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 @dataclass
 class RiskWeightConfig:
     """
-    Risk weights for each portion of our pipeline
+    Risk weights for each portion of our pipeline.
     """
-    def __init__(self, weight_cme, weight_nlp, weight_gnn):
+    def __init__(self,weight_cme: float = 0.45, weight_nlp: float = 0.25, weight_gnn: float = 0.30):
         self.weights_cme = weight_cme
         self.weights_nlp = weight_nlp
         self.weights_gnn = weight_gnn
